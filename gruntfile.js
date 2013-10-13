b17a582e5be03726308a9507ad4a330791220fcf
Sarah <sarah@sarahhenderson.info>
2013-10-13 23:35:32 +0530

    Updated icons and documentation

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,18 @@
 module.exports = function (grunt) {
    // Do grunt-related things in here
 
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+   grunt.loadNpmTasks('grunt-contrib-clean');
+   grunt.loadNpmTasks('grunt-contrib-compress');
+
    grunt.initConfig({
       pkg: grunt.file.readJSON("package.json"),
 
       jshint: {
-         // define the files to lint
          files: ['gruntfile.js', 'js/*.js'],
-         // configure JSHint (documented at http://www.jshint.com/docs/)
          options: {
             globals: {
                console: true,
@@ -17,16 +22,15 @@ module.exports = function (grunt) {
          }
       },
 
-    uglify: {
-        options: {
+      uglify: {
+         options: {
         // the banner is inserted at the top of the output
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> /*\n',
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
             mangle: {
                except: ['jQuery']
             }
-
-        },
-        dev: {
+         },
+         build: {
             files: [
                  {
                     expand: true,     // Enable dynamic expansion.
@@ -36,47 +40,62 @@ module.exports = function (grunt) {
                     ext: '.min.js',   // Dest filepaths will have this extension.
                  },
             ],
+         },
+         www: {
+            files: [
+                 {
+                    expand: true,     // Enable dynamic expansion.
+                    cwd: 'gh-pages/js',      // Src matches are relative to this path.
+                    src: ['*.js', '!*.min.js'], // Actual pattern(s) to match.
+                    dest: 'gh-pages/js',   // Destination path prefix.
+                    ext: '.min.js',   // Dest filepaths will have this extension.
+                 },
+            ],
          }
-    },
+
+      },
     
-    cssmin: {
-        minify: {
+      cssmin: {
+         build: {
             expand: true,
             cwd: 'src/css',
             src: ['*.css', '!*.min.css'],
             dest: 'src/css',
             ext: '.min.css'
-        }
-    },
-
-    copy: {
-        main: {
-            files: [
-              {
-                 expand: true,
-                 cwd: 'src/',
-                 src: ['**/*.min.css', '**/*.min.js', '**/*.php', '**/*.png', '**/*.jpg', '**/*.txt', '**/*.pot'],
-                 dest: 'svn/'
-              }]
+         },
+         www: {
+            expand: true,
+            cwd: 'gh-pages/css',
+            src: ['*.css', '!*.min.css'],
+            dest: 'gh-pages/css',
+            ext: '.min.css'
          }
-    },
-    
-    'gh-pages': {
-        options: {
-            base: 'docs'
-        },
-        src: ['**']
-    }
-    
-    });
+      },
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-gh-pages');
+      copy: {
+         build: {
+            files: [{
+               expand: true,
+               cwd: 'src/',
+               src: ['**/*.min.css', '**/*.min.js', '**/*.php', '**/*.png', '**/*.jpg', '**/*.txt', '**/*.pot'],
+               dest: 'svn/'
+            }]
+         }
+      },
+    
+      clean: {
+         build: ["svn/*"]
+      }
 
-    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'copy']);
-    grunt.registerTask('dev', ['jshint', 'uglify', 'cssmin']);
-    grunt.registerTask('pages', ['gh-pages']);
+   });
+
+   grunt.registerTask('default', 
+      ['jshint', 'uglify', 'cssmin', 'copy', 'compress']);
+      
+   grunt.registerTask('build', 
+      ['jshint', 'uglify:build', 'cssmin:build', 'clean:build', 'copy:build', 'compress:build' ]);   
+      
+   grunt.registerTask('www', 
+      ['jshint', 'uglify:www', 'cssmin:www']);
+      
 };
